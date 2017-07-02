b5c7daf9ddd31e6d8d7073212ceebf7ec5975993
sureshtaritedevops <sureshtaritedevops@gmail.com>
2017-07-02 23:32:42 +0530

    Update sample.java

--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -4,6 +4,8 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Helloworld++++++***********");
+		System.out.println("suresh");
+		
 	}
 
 }
